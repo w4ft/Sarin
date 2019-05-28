90ff6b89ccaaa8ef2ff6b70038a1cf16b84d26e3
w4ft <joshzhe@josh-z.local>
2019-05-28 16:27:09 -0400

    fixed cp

--- sarin_scripts/httrack.sh
+++ sarin_scripts/httrack.sh
@@ -3,4 +3,4 @@ httrack "https://www.$1" -O "./www.$1" "https://www.$1" -v -s0  --depth=1 -n
 cd "www.$1/www.$1"
 cp index.html "/$2/www.$1"
 echo $3 | sudo -S rm -r /Library/WebServer/Documents/*
-echo $3 | sudo -S cp "/$2/www.$1/*" /Library/WebServer/Documents/
+echo $3 | sudo -S cp -a "/$2/www.$1/." /Library/WebServer/Documents/
