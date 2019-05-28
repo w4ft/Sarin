5896c63b5e28b06c7a28c54831ca417607fbffbb
w4ft <joshzhe@josh-z.local>
2019-05-28 16:22:46 -0400

    added sudo

--- sarin_scripts/httrack.sh
+++ sarin_scripts/httrack.sh
@@ -2,5 +2,5 @@ cd $2
 httrack "https://www.$1" -O "./www.$1" "https://www.$1" -v -s0  --depth=1 -n
 cd "www.$1/www.$1"
 cp index.html "/$2/www.$1"
-rm -r /Library/WebServer/Documents/*
+echo $3 | rm -r /Library/WebServer/Documents/*
 echo $3 | sudo -S cp "/$2/*" /Library/WebServer/Documents/
