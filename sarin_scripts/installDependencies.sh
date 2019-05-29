ec93df60ef3ce350d098886ad2f2fa450db916c2
w4ft <joshzhe@josh-z.local>
2019-05-29 00:57:48 -0400

    added pagers and nodeJS to install

--- sarin_scripts/installDependencies.sh
+++ sarin_scripts/installDependencies.sh
@@ -1,2 +1,3 @@
-brew install berkeley-db libnet libnids libpcap openssl arp-scan httrack expect
+brew install berkeley-db libnet libnids libpcap openssl arp-scan httrack expect node
+echo $2 | sudo -S npm install --global pageres-cli
 mkdir $1
