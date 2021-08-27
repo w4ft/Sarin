32b8cc53c3e3219dddd8cc8944c0bb50b61bc7d5
w4ft <42308831+w4ft@users.noreply.github.com>
2021-08-26 22:57:42 -0400

    Update arp-scan.sh

--- sarin_scripts/arp-scan.sh
+++ sarin_scripts/arp-scan.sh
@@ -1,1 +1,1 @@
-echo $1 | sudo -S arp-scan --interface=en0 --localnet  | tail +3 | grep -v Ending | grep -v packets | grep -v '^$'
+echo $1 | sudo -S /opt/homebrew/bin/arp-scan --interface=en0 --localnet  | tail +3 | grep -v Ending | grep -v packets | grep -v '^$'
